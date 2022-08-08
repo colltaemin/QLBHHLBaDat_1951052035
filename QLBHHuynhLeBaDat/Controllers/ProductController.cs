41dd039ad134b0ec4badba826b51ea46f612acf2
colltaemin <1951052035dat@ou.edu.vn>
2022-08-08 21:14:24 +0700

    first commit

---
+++ QLBHHuynhLeBaDat/Controllers/ProductController.cs
@@ -0,0 +1,78 @@
+﻿using QLBHHuynhLeBaDat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLBHHuynhLeBaDat.Controllers
+{
+    public class ProductController : Controller
+    {
+        NWDataClassesDataContext da = new NWDataClassesDataContext();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var ps = da.Products.OrderByDescending(s=>s.ProductID).ToList();
+            return View(ps);
+        }
+
+        public ActionResult Create()
+        {
+            ViewData["SupplierID"] = new SelectList(da.Suppliers, "SupplierID", "CompanyName");
+            ViewData["CategoryID"] = new SelectList(da.Categories, "CategoryID", "CategoryName");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            da.Products.InsertOnSubmit(product);
+            da.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var product = da.Products.First(p => p.ProductID == id);
+            return View(product);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var product = da.Products.First(p => p.ProductID == id);
+
+            ViewData["SupplierID"] = new SelectList(da.Suppliers, "SupplierID", "CompanyName", product.SupplierID);
+            ViewData["CategoryID"] = new SelectList(da.Categories, "CategoryID", "CategoryName", product.CategoryID);
+
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection form)
+        {
+            var product = da.Products.First(p => p.ProductID == id);
+            product.ProductName = form["ProductName"];
+            da.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var product = da.Products.First(p => p.ProductID == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection form)
+        {
+            var product = da.Products.First(p => p.ProductID == id);
+            da.Products.DeleteOnSubmit(product);
+            da.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ QLBHHuynhLeBaDat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(QLBHHuynhLeBaDat.Startup))]
+namespace QLBHHuynhLeBaDat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
